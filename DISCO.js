fddc12645b117d4e09490d49da4bc1bac528c3fe
Giovanni Puntil <giovanni.puntil@asos.com>
2016-05-05 20:02:28 +0100

    new categories

--- DISCO.js
+++ DISCO.js
@@ -1,6 +1,6 @@
 (function(){
     var DISCO = function(){
-        
+
         this.isProductPage;
         this.isCategoryPage;
         this.isSearchPage;
@@ -10,59 +10,59 @@
         this.Gender;
         this.isEnabled;
         this.miniCartTotal;
-        
+
         this.location;
         this.priceContainer;
         this.currency ;
         this.dfd = jQuery.Deferred();
         this.doneOnce = false;
-        
+
         this.dfd.promise(this)
         this.done(this.continueLoading.bind(this));
-        
+
         this.getStatus();
     };
-    
-    
+
+
     DISCO.prototype.getStatus = function(){
         chrome.runtime.sendMessage( {method: "getLocalStorage", key: "asos_disco"}, this.setStatus.bind(this) );
     }
-    
+
     DISCO.prototype.setStatus = function(response){
         this.isEnabled = ( response.data === "true" || typeof(response.data) === "undefined" );
         this.dfd.resolve( "done" );
     }
-    
+
     DISCO.prototype.continueLoading = function(){
         if(this.isEnabled){
             this.fetchPageType();
             this.selectItems();
             this.applyDiscountMiniCart();
-        
+
             if(!this.doneOnce)
                 this.doneOnce = true;
             this.bindEvents();
         }
     }
-    
+
     DISCO.prototype.bindEvents = function(){
-        
+
         if(this.isSearchPage){
             $(document).delegate(".results > ul", "DOMSubtreeModified", this.onChangeDiscountSearchPage.bind(this));
-            $(".quickview-panel .current-price").on("DOMSubtreeModified", this.applyDiscountQuickView.bind(this));
         }else if(this.isCategoryPage){
             $(document).delegate("div#items-wrapper", "DOMSubtreeModified", this.onChangeDiscountCategoryPage.bind(this));
-            $("#quickViewPopup .current-price").on("DOMSubtreeModified", this.applyDiscountQuickView.bind(this));
         } else if(this.isCartPage) {
             $(document).delegate("#totalAmount", "DOMSubtreeModified", this.applyDiscountCartPage.bind(this));
         }
+        $("#quickViewPopup .current-price").on("DOMSubtreeModified", this.applyDiscountQuickView.bind(this));
+        $(".quickview-panel .current-price").on("DOMSubtreeModified", this.applyDiscountQuickView.bind(this));
         $("#miniBasket .total").on("DOMSubtreeModified", this.applyDiscountMiniCart.bind(this));
-        
+
     }
-    
+
     DISCO.prototype.fetchPageType = function(){
         this.location = window.location.href.toLowerCase();
-        
+
         this.isBeautyProduct = this.detectBeautyPage();
         this.Gender = this.detectPageGender();
         this.isProductPage = this.location.indexOf("pgeproduct.aspx") > -1;
@@ -71,39 +71,39 @@
         this.isCartPage = this.location.indexOf("pgebasket.aspx") > -1;
         this.isSavedItemsPage = this.location.indexOf("pgesavedlist.aspx") > -1;
     }
-    
+
     DISCO.prototype.detectBeautyPage = function(){
         var isBeauty = false;
-        
+
         if(this.location.indexOf("/beauty/") > -1){
             isBeauty = true;
         }else if(document.getElementById("CategoryName")){
             isBeauty = document.getElementById("CategoryName").value.toLowerCase() == "beauty";
         }
-        
+
         return isBeauty;
     }
-    
+
     DISCO.prototype.detectPageGender = function(){
         var isWomen = "";
-        
+
         if(this.location.indexOf("/women/") > -1){
             isWomen = "women";
         }else if(document.getElementById("FloorName")){
             isWomen = document.getElementById("FloorName").value.toLowerCase();
         }
-        
+
         return isWomen;
     }
-    
+
     DISCO.prototype.isDiscountApplied = function(){
         return this.priceContainer.find(".disco-applied").length;
     }
 
     DISCO.prototype.selectItems = function(){
-        
+
         if (this.isProductPage) {
-            
+
             if($(".outlet-current-price").length){
                 this.priceContainer = $(".outlet-current-price");
             }else if($(".previousprice").length){
@@ -111,13 +111,13 @@
             }else{
                 this.priceContainer = $(".product_price_details");
             }
-            
+
             this.applyDiscountPDP();
         }else if(this.isSearchPage){
             this.priceContainer = $(".price-wrap.price-current");
             this.applyDiscountSearchPage();
         }else if(this.isCategoryPage){
-            this.priceContainer = $("ul#items .productprice");
+            this.priceContainer = $("ul#items .productprice").length ? $("ul#items .productprice") : $(".price-wrap.price-current");
             this.applyDiscountCategoryPage();
         }else if(this.isCartPage){
             this.priceContainer = $("#totalAmount");
@@ -126,13 +126,14 @@
             this.priceContainer = $("ul#sortable li");
             this.applyDiscountSavedItemPage();
         }
-        
+
+        this.applyDiscountYMAL();
     };
-    
+
     DISCO.prototype.fetchCurrency = function(i){
         if(this.priceContainer) {
             var pricetext = this.priceContainer.eq(i).text();
-        
+
             if(pricetext.indexOf("£") != -1){
                 this.currency = "£";
             }else if(pricetext.indexOf("$") != -1){
@@ -144,13 +145,16 @@
             }
         }
     }
-    
+
+    DISCO.prototype.applyDiscountYMAL = function(){
+    }
+
     DISCO.prototype.applyDiscountPDP = function(){
         //Product Page
         var discountFactor = ((this.Gender == "women") && this.isBeautyProduct) ? 0.8 : 0.6;
-        
+
         if(typeof(this.priceContainer) !== "undefined"){
-            
+
             for(var i = (this.priceContainer.length - 1); i >= 0; i--){
                 this.fetchCurrency(i);
                 var current_price_dom = this.priceContainer.eq(i);
@@ -165,9 +169,9 @@
                         inner_html += "</div>";
 
                         //current_price_dom.hide();
-                        $(".discounted-price").hide();
+                        //$(".discounted-price").hide();
                         //current_price_dom.hide();
-                        
+
                         current_price_dom.css("color", "#000");
                         current_price_dom.after(inner_html);
                     }
@@ -175,20 +179,20 @@
             }
         }
     }
-    
+
     DISCO.prototype.applyDiscountQuickView = function(){
         //Quick View
         var discountFactor = ((this.Gender == "women") && this.isBeautyProduct) ? 0.8 : 0.6;
-        
+
         if(typeof(this.priceContainer) !== "undefined"){
-            this.priceContainer = this.isSearchPage ? $(".quickview-panel .current-price") : $("#quickViewPopup .current-price");
+            this.priceContainer = $(".quickview-panel .current-price").length ? $(".quickview-panel .current-price") : $("#quickViewPopup .current-price");
             $(".disco-applied.quick-view").remove();
             for(var i = (this.priceContainer.length - 1); i >= 0; i--){
                 this.fetchCurrency(i);
                 var current_price_dom = this.priceContainer.eq(i);
                 var price = parseFloat( current_price_dom.text().replace(this.currency, "").replace("NOW", "").replace("FROM", "")  );
                 var after_price = (price * discountFactor).toFixed(2);
-                
+
                 if(!isNaN(after_price)){
                     var inner_html = "<div class='disco-applied quick-view' style='margin:5px 0;font-size:13px;font-weight:bold;'>";
                     inner_html += "<div style='color:#900;margin:0;padding:0;height:16px;'>with ASOS discount</div>" + "<div style='color:#900;margin:0;padding:0;height:16px;'>"+ this.currency + after_price + "</div>";
@@ -202,18 +206,18 @@
             }
         }
     }
-    
+
     DISCO.prototype.onChangeDiscountCategoryPage = function(){
         this.priceContainer = $("ul#items .productprice");
         this.applyDiscountCategoryPage();
     }
-    
+
     DISCO.prototype.applyDiscountCategoryPage = function(){
         //Category
         var discountFactor = ((this.Gender == "women") && this.isBeautyProduct) ? 0.8 : 0.6;
-        
+
         if(!this.isDiscountApplied() && typeof(this.priceContainer) !== "undefined"){
-            
+
             for(var i = (this.priceContainer.length - 1); i >= 0; i--){
                 this.fetchCurrency(i);
                 if(!this.priceContainer.eq(i).find(".disco-applied").length){
@@ -229,7 +233,7 @@
 
                     var price = parseFloat( current_price_dom.text().replace(this.currency, "").replace("NOW", "").replace("FROM", "")  );
                     var after_price = (price * discountFactor).toFixed(2);
-                    
+
                     if(!isNaN(after_price)){
                         var inner_html = "<div class='disco-applied'>";
                         inner_html += "<div>ASOS Staff price</div>";
@@ -238,27 +242,27 @@
                         inner_html += "</div>";
                         current_price_dom.hide();
                         this.priceContainer.eq(i).find(".rrp, .prevPrice, .previousprice, .disco-applied").hide();
-                    
+
                         current_price_dom.after(inner_html);
                     }
                 }
             }
         }
     }
-    
+
     DISCO.prototype.onChangeDiscountSearchPage = function(){
         this.priceContainer = $(".price-wrap.price-current");
         this.applyDiscountSearchPage();
     }
-    
+
     DISCO.prototype.applyDiscountSearchPage = function(){
         //Search Results
-        
+
         if(typeof(this.priceContainer) !== "undefined"){
-            
+
             for(var i = (this.priceContainer.length - 1); i >= 0; i--){
                 this.fetchCurrency(i);
-                
+
                 if(!this.priceContainer.eq(i).find(".disco-applied").length){
                     var current_price_dom = undefined;
 
@@ -272,7 +276,7 @@
 
                     var price = parseFloat( current_price_dom.text().replace(this.currency, "").replace("NOW", "").replace("FROM", "")  );
                     var after_price = (price * 0.6).toFixed(2);
-                    
+
                     if(!isNaN(after_price)){
                         var inner_html = "<div class='disco-applied'>";
                         inner_html += "<div>ASOS Staff price</div>";
@@ -284,25 +288,25 @@
 
                         current_price_dom.after(inner_html);
                     }
-                    
+
                 }
             }
         }
     }
-    
+
     DISCO.prototype.applyDiscountSavedItemPage = function(){
         //Saved Item
         if(!this.isDiscountApplied() && typeof(this.priceContainer) !== "undefined"){
-            
+
             for(var i = (this.priceContainer.length - 1); i >= 0; i--){
                 this.fetchCurrency(i);
                 var current_price_dom = undefined;
-                
+
                 current_price_dom = this.priceContainer.eq(i).find(".product-price");
-                
+
                 var price = parseFloat( current_price_dom.text().replace(this.currency, "").replace("NOW", "").replace("FROM", "")  );
                 var after_price = (price * 0.6).toFixed(2);
-                
+
 
                 if(!isNaN(after_price)){
                     var inner_html = "<div class='disco-applied'>";
@@ -318,14 +322,14 @@
             }
         }
     }
-    
+
     DISCO.prototype.applyDiscountCartPage = function(){
         //Basket
         this.fetchCurrency(0);
         var current_price_dom = this.priceContainer.eq(0);
         var price = parseFloat( current_price_dom.text().replace(this.currency, "")  );
         var after_price = (price * 0.6).toFixed(2);
-        
+
         if(!isNaN(after_price)){
             var inner_html = "<span style='color:#900;'>"+ this.currency + after_price + "</span>";
             current_price_dom.hide();
@@ -333,24 +337,24 @@
 
             $("dt.total")[0].innerHTML = "Total Cost with ASOS discount";
             $("dd.total").after("<dt class='you-save total' style='border-top:0px;color: green;margin:0;padding:0;'>You Save</dt>");
-            $("dt.you-save").after("<dd class='total' style='border-top:0px;color: green;margin:0;padding:0;'><span style='padding-right:14px;'>" + ( this.currency + (price - after_price).toFixed(2) ) + "</span></dd>")  
+            $("dt.you-save").after("<dd class='total' style='border-top:0px;color: green;margin:0;padding:0;'><span style='padding-right:14px;'>" + ( this.currency + (price - after_price).toFixed(2) ) + "</span></dd>")
         }
     }
-    
+
     DISCO.prototype.applyDiscountMiniCart = function(){
-        //Mini Cart 
-        
+        //Mini Cart
+
         this.fetchCurrency(0);
         var current_price_dom = $("#miniBasket .total");
         var price = parseFloat( current_price_dom.text().replace(this.currency, "")  );
         var after_price = (price * 0.6).toFixed(2);
-        
+
         if(!this.doneOnce){
             current_price_dom.before("<span class='asos-mini-disco'> with ASOS discount</span>");
             this.miniCartTotal = price;
         }
-        
-        
+
+
         if(!isNaN(price) && (price != this.miniCartTotal || !this.doneOnce) ){
             //console.log("price", price);
             current_price_dom.next(".new-total").remove();
